33b67081daf8095cae0b2a5b7501fc144d402be5
Nina Rojanek <nina.rojanek@globallogic.com>
2022-08-05 13:30:17 +0200

    added DOCKERFILE

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 //const date = require(__dirname + "/date.js");
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.set('view engine', 'ejs');
 
